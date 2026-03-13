df28a0ac816b26bdef971f25c6f19d345a2f824f
Công Nghĩa <82830416+conghiale@users.noreply.github.com>
2026-03-13 21:56:14 +0700

    Preparing for deployment and testing. API shipment is being processed.

---
+++ src/main/java/org/ArtAndDecor/config/StartupLogger.java
@@ -0,0 +1,14 @@
+package org.ArtAndDecor.config;
+
+/**
+ * Project: ArtAndDecor
+ * Date: 2026/03/13
+ * Time: 3:37 PM
+ */
+ 
+ /** 
+ * @ 2026. All rights reserved
+ */
+    
+    public class StartupLogger {
+}
